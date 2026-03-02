b18f0bc2a84005d19ba94a23d023e24dcc49f05b
aether-flux <work.amar05@yahoo.com>
2026-03-03 02:38:55 +0530

    feat: add themes and wallpapers e-ink and jade night

--- scripts/.scripts/general/theme_switcher.sh
+++ scripts/.scripts/general/theme_switcher.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-# choice=$(printf "%s\n" \
-#   "Everforest" \
-#   "Nord" \
-#   "Gruvbox" \
-#   "Catppuccin" | rofi -dmenu -i -p "Switch Theme" -theme ~/.config/rofi/launchers/project/theme_switcher.rasi)
-
 choice=$(printf "%s\n" \
   "Everforest" \
+  "Jade Night" \
   "Nord" \
   "Gruvbox" \
+  "E-Ink" \
   "Scarlet" \
   "Catppuccin" | rofi -dmenu \
      -theme ~/.config/rofi/appLauncher/project/base.rasi \
      -theme-str '* { placeholder-text: "Select Theme..."; }')
 
-# rofi -show drun \
-#      -theme ~/.config/rofi/base.rasi \
-#      -theme-str '* { placeholder-text: "Select Theme..."; }'
-
 case $choice in
   "Everforest")
     cp -r $HOME/.config/themes/everforest/* $HOME/.config/themes/current/
     awww img $HOME/Pictures/Wallpapers/ever_1 --transition-type random & disown
+    paplay ~/Documents/Sounds/theme_switch.mp3 --volume=32768 &
+    hyprctl reload
+    killall waybar
+    waybar & disown
+    ;;
+  "Jade Night")
+    cp -r $HOME/.config/themes/jade_night/* $HOME/.config/themes/current/
+    awww img $HOME/Pictures/Wallpapers/jade_night_1.jpg --transition-type random & disown
+    paplay ~/Documents/Sounds/theme_switch.mp3 --volume=32768 &
     hyprctl reload
     killall waybar
     waybar & disown
@@ -30,6 +31,7 @@ case $choice in
   "Nord")
     cp -r $HOME/.config/themes/nord/* $HOME/.config/themes/current/
     awww img $HOME/Pictures/Wallpapers/nord_1.jpg --transition-type random & disown
+    paplay ~/Documents/Sounds/theme_switch.mp3 --volume=32768 &
     hyprctl reload
     killall waybar
     waybar & disown
@@ -37,13 +39,15 @@ case $choice in
   "Gruvbox")
     cp -r $HOME/.config/themes/gruvbox/* $HOME/.config/themes/current/
     awww img $HOME/Pictures/Wallpapers/gruvbox_1.jpg --transition-type random & disown
+    paplay ~/Documents/Sounds/theme_switch.mp3 --volume=32768 &
     hyprctl reload
     killall waybar
     waybar & disown
     ;;
-  "Catppuccin")
-    cp -r $HOME/.config/themes/catppuccin/* $HOME/.config/themes/current/
-    awww img $HOME/Pictures/Wallpapers/catppuccin_1.jpg --transition-type random & disown
+  "E-Ink")
+    cp -r $HOME/.config/themes/eink/* $HOME/.config/themes/current/
+    awww img $HOME/Pictures/Wallpapers/eink_1.jpg --transition-type random & disown
+    paplay ~/Documents/Sounds/theme_switch.mp3 --volume=32768 &
     hyprctl reload
     killall waybar
     waybar & disown
@@ -51,6 +55,15 @@ case $choice in
   "Scarlet")
     cp -r $HOME/.config/themes/scarlet/* $HOME/.config/themes/current/
     awww img $HOME/Pictures/Wallpapers/scarlet_1.jpg --transition-type random & disown
+    paplay ~/Documents/Sounds/theme_switch.mp3 --volume=32768 &
+    hyprctl reload
+    killall waybar
+    waybar & disown
+    ;;
+  "Catppuccin")
+    cp -r $HOME/.config/themes/catppuccin/* $HOME/.config/themes/current/
+    awww img $HOME/Pictures/Wallpapers/catppuccin_1.jpg --transition-type random & disown
+    paplay ~/Documents/Sounds/theme_switch.mp3 --volume=32768 &
     hyprctl reload
     killall waybar
     waybar & disown
